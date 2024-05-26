200bc18b2f5e5d1db0baaae7210439f3ee2d1306
alptekinmh <alptekinmehmet23@gmail.com>
2024-05-26 16:30:17 +0300

    google cloud kullanilarak API eklendi

--- dietLife/settings.py
+++ dietLife/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+API_KEY = "AIzaSyAx7K5mTFqGfrGLEmJ8nH7gdhgfkKlOuUg"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
