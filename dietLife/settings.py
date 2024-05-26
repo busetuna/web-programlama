a02f42b45ea84ae5a7eb48282d00bd101572e3ee
alptekinmh <alptekinmehmet23@gmail.com>
2024-05-26 14:35:09 +0300

    localhost ve http ile guvenlik saglandi

--- dietLife/settings.py
+++ dietLife/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-_-zv$h#=@dtrq&c7#p!y*46ld-4-hehyn_5g$$nlp#bt_c3lcw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+  # localhost eklenmiş
 
 # Application definition
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
